adc468fa9ba38d306a47bec1aa2a086eae8a11c8
JinWYP <jinwyp@gmail.com>
2024-11-01 06:00:45 +0800

    U-update all CF ip

--- bestdomain.py
+++ bestdomain.py
@@ -44,16 +44,16 @@ def update_cloudflare_dns(ip_list, api_token, zone_id, subdomain, domain):
 
     tempCounter = 0
     for ip in ip_list:
-        data = {
-            "type": "A",
-            "name": record_name,
-            "content": ip,
-            "ttl": 1,
-            "proxied": False
-        }
-        
         tempCounter += 1
+        # 每个子域名只添加一条A记录
         if tempCounter < 2:
+            data = {
+                "type": "A",
+                "name": record_name,
+                "content": ip,
+                "ttl": 1,
+                "proxied": False
+            }
             print(f"Prepare to add {record_name}:{ip}")
             response = requests.post(f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records', json=data, headers=headers)
             if response.status_code == 200:
@@ -67,9 +67,24 @@ if __name__ == "__main__":
 
     # 示例URL和子域名对应的IP列表
     subdomain_ip_mapping = {
-        #'bestcf': 'https://ipdb.030101.xyz/api/bestcf.txt',
-        'cfname1': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip.txt',
-        'cfname2': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip.txt',
+        'cfsite1cmx1': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site1_CM.txt',
+        'cfsite1cmx2': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site1_CM.txt',
+        'cfsite1ctx1': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site1_CT.txt',
+        'cfsite1ctx2': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site1_CT.txt',
+        'cfsite1cux1': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site1_CU.txt',
+        'cfsite1cux2': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site1_CU.txt',
+
+        'cfsite2cmx1': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site2_CM.txt',
+        'cfsite2cmx2': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site2_CM.txt',
+        'cfsite2ctx1': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site2_CT.txt',
+        'cfsite2ctx2': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site2_CT.txt',
+
+        'cfsite3x1': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site3.txt',
+        'cfsite3x2': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site3.txt',
+
+        'cfsite4x1': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site4.txt',
+        'cfsite4x2': 'https://raw.githubusercontent.com/jinwyp/youxuanyuming/refs/heads/main/ip_site4.txt',
+
         # 添加更多子域名和对应的IP列表URL
     }
     
@@ -82,6 +97,14 @@ if __name__ == "__main__":
         for subdomain, url in subdomain_ip_mapping.items():
             # 获取IP列表
             ip_list = get_ip_list(url)
+
+            last_char = subdomain[-1]
+            if last_char.isdigit():
+                print(f"last_char: {last_char}")
+                num_to_reduce = int(last_char)
+                
+                if num_to_reduce < len(ip_list):
+                    ip_list = ip_list[num_to_reduce:]  # 从头部减少 num_to_reduce 个元素
 
             # 删除现有的DNS记录
             delete_existing_dns_records(api_token, zone_id, subdomain, domain)
--- collect_ips.py
+++ collect_ips.py
@@ -1,12 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+
+
+import pyppeteer
 import re
 import os
+import asyncio
+
+# 设置 Chromium 路径
+pyppeteer.launcher.CHROME_PATH = 'D:/github/chrome/chrome-win/chrome.exe'
+
 
 # 目标URL列表
-urls = ['https://ip.164746.xyz/ipTop10.html', 
-        'https://cf.090227.xyz'
+urls = [
+        'https://stock.hostmonit.com/CloudFlareYes',
+        'https://cf.090227.xyz',
+        'https://ip.164746.xyz/ipTop10.html', 
+        'https://ipdb.030101.xyz/api/bestcf.txt',
         ]
+
 
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
@@ -15,46 +27,111 @@ ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 if os.path.exists('ip.txt'):
     os.remove('ip.txt')
 
-ip_matches = []
-for url in urls:
-    # 发送HTTP请求获取网页内容
-    response = requests.get(url)
-    
-    # 使用BeautifulSoup解析HTML
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # 根据网站的不同结构找到包含IP地址的元素
-    if url == 'https://ip.164746.xyz/ipTop10.html':
-        elements = response.text.split(',')
-    elif url == 'https://cf.090227.xyz':
-        elements = soup.find_all('tr')
-    else:
-        elements = soup.find_all('li')
-    
-    # print(f'URL: {url} ')
-    # print(f'Html: {elements} ')
-    
-    # 遍历所有元素,查找IP地址
 
-    for element in elements:
-        if hasattr(element, 'get_text'):
-            for tdone in element.find_all('td'):
-                td_text = tdone.get_text()
-                if re.match(ip_pattern, td_text):
-                    ip_matches.append(td_text)
-                    # print(f'IP地址: {td_text}')
+async def fetch_dynamic_content(url):
+    browser = await pyppeteer.launch(
+        executablePath="D:/github/chrome/chrome-win/chrome.exe",  # 替换为 Chromium 可执行文件的路径
+        headless=True,
+        args=['--no-sandbox', '--disable-setuid-sandbox']
+    )
+
+    page = await browser.newPage()
+    await page.goto(url)
+    await asyncio.sleep(5)  # 等待5秒钟
+    content = await page.content()
+    await browser.close()
+    return content
+
+
+async def main():
+    ip_ListAll = []
+    for index, url in enumerate(urls):
+
+        if url == 'https://stock.hostmonit.com/CloudFlareYes':
+            html_content = await fetch_dynamic_content(url)
+            # print(html_content)
         else:
-            if re.match(ip_pattern, element):
-                ip_matches.append(element)
-                # print(f'IP地址: {element}')
+            # 发送HTTP请求获取网页内容
+            response = requests.get(url)
+            html_content = response.text
+
+        # 使用BeautifulSoup解析HTML
+        soup = BeautifulSoup(html_content, 'html.parser')
+        
+        elements = []
+        # 根据网站的不同结构找到包含IP地址的元素
+        if url == 'https://cf.090227.xyz':
+            elements = soup.find_all('tr')
+        elif url == 'https://stock.hostmonit.com/CloudFlareYes':
+            elements = soup.find_all('tr')
+
+        elif url == 'https://ip.164746.xyz/ipTop10.html' :
+            elements = html_content.split(',')
+        elif url == 'https://ipdb.030101.xyz/api/bestcf.txt':
+            elements = html_content.split('\n')
+        else:
+            elements = soup.find_all('li')
 
 
-# 创建一个文件来存储IP地址
-with open('ip.txt', 'w') as file:
-    
-    # 如果找到IP地址,则写入文件
-    for ip in ip_matches:
-        file.write(ip + '\n')
+        print(f'\n URL: {url} ')
+        # print(f'Html: {elements} ')
+        
+        # 遍历所有元素,查找IP地址
+        ip_ListSingleUrl = []
+        ip_ListCM = []
+        ip_ListCU = []
+        ip_ListCT = []
+        for element in elements:
+            if hasattr(element, 'get_text'):
+                tdsOneRow = element.find_all('td')
+                for tdone in tdsOneRow:
+                    td_text = tdone.get_text()
+                    # print(f'IP地址td: {td_text}')
+                    if re.match(ip_pattern, td_text):
+                        ip_ListAll.append(td_text)
+                        ip_ListSingleUrl.append(td_text)
+
+                if len(tdsOneRow) > 1:
+                    if tdsOneRow[0].get_text() == '移动':
+                        ip_ListCM.append(tdsOneRow[1].get_text())
+                    if tdsOneRow[0].get_text() == '联通':
+                        ip_ListCU.append(tdsOneRow[1].get_text())
+                    if tdsOneRow[0].get_text() == '电信':
+                        ip_ListCT.append(tdsOneRow[1].get_text())
+
+            else:
+                if re.match(ip_pattern, element):
+                    ip_ListAll.append(element)
+                    ip_ListSingleUrl.append(element)
+                    print(f'IP地址text: {element}')
+
+        if len(ip_ListCM) > 0:
+            with open(f'ip_site{index + 1}_CM.txt', 'w', encoding='utf-8') as file:
+                for ip in ip_ListCM:
+                        file.write(ip + '\n')
+
+        if len(ip_ListCU) > 0:
+            with open(f'ip_site{index + 1}_CU.txt', 'w', encoding='utf-8') as file:
+                for ip in ip_ListCU:
+                        file.write(ip + '\n')
+
+        if len(ip_ListCT) > 0:
+            with open(f'ip_site{index + 1}_CT.txt', 'w', encoding='utf-8') as file:
+                for ip in ip_ListCT:
+                        file.write(ip + '\n')
+
+        if len(ip_ListSingleUrl) > 0:
+            with open(f'ip_site{index + 1}.txt', 'w', encoding='utf-8') as file:
+                for ip in ip_ListSingleUrl:
+                        file.write(ip + '\n')
+
+    # 创建一个文件来存储IP地址
+    with open('ip.txt', 'w', encoding='utf-8') as file:
+        for ip in ip_ListAll:
+            file.write(ip + '\n')
 
 
-print('IP地址已保存到ip.txt文件中。')
+    print('\n IP地址已保存到ip.txt文件中 \n')
+
+# 运行主函数
+asyncio.get_event_loop().run_until_complete(main())
